bdc3e7f97f94b1122948670a1b117ab2cf219189
Mihir Shete <mihirsht@gmail.com>
2015-09-10 23:33:46 -0500

    hw: cs537: pointer + parallel qsort test

--- cs537/pa1/rangesort/rangesort.c
+++ cs537/pa1/rangesort/rangesort.c
@@ -12,6 +12,11 @@
 #include <unistd.h>
 #include <limits.h>
 
+typedef struct __srec_t {
+    unsigned int key;
+    int (*precord)[NUMRECS];
+} srec_t;
+
 void
 usage(char *prog) 
 {
@@ -23,9 +28,9 @@ usage(char *prog)
 int
 compar_f(const void *a, const void *b)
 {
-    if (((rec_t *)a)->key < ((rec_t *)b)->key)
+    if (((srec_t *)a)->key < ((srec_t *)b)->key)
         return -1;
-    else if (((rec_t *)a)->key == ((rec_t *)b)->key)
+    else if (((srec_t *)a)->key == ((srec_t *)b)->key)
         return 0;
     else
         return 1;
@@ -38,10 +43,11 @@ main(int argc, char *argv[])
     char *inFile = "/no/such/file";
     char *outFile = "/no/such/file";
     // input params
-    int c, fd, pid, status, psize;
+    int c, fd, pid, status, psize, ofd;
     opterr = 0;
     struct stat fs;
-    rec_t *r, *rn1, *rn2;
+    rec_t *r;
+    srec_t *rn1, *rn2;
     char *fp, *dst, *f, *fp1, *fp2;
 	int rc, j, set = 0, count1 = 0, count2 = 0;
     unsigned int i;
@@ -80,27 +86,20 @@ main(int argc, char *argv[])
 	exit(1);
     }
     // open and create output file
-    fd = open(inFile, O_RDONLY);
-    if (fd < 0) {
+    ofd = open(inFile, O_RDONLY);
+    if (ofd < 0) {
 	perror("open");
 	exit(1);
     }
 
-    if (fstat(fd, &fs) == -1) {
+    if (fstat(ofd, &fs) == -1) {
         exit(errno);
     }
 
-    fp1 = (char *)malloc(fs.st_size);
-    fp2 = (char *)malloc(fs.st_size);
+    fp1 = (char *)malloc((fs.st_size / sizeof(rec_t)) * sizeof(srec_t));
+    fp2 = (char *)malloc((fs.st_size / sizeof(rec_t)) * sizeof(srec_t));
 
-#if 0
-	rc = read(fd, fp, fs.st_size);
-	if (rc < 0) {
-	    perror("read");
-	    exit(1);
-	}
-#endif
-    f = mmap(NULL, fs.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    f = mmap(NULL, fs.st_size, PROT_READ, MAP_SHARED, ofd, 0);
     madvise(f, fs.st_size, MADV_SEQUENTIAL);
 
     if (f == NULL) {
@@ -119,26 +118,27 @@ main(int argc, char *argv[])
     median = max/2;
     for (i = 0; i<=highindex; i++) {
         r = (rec_t *)(f + i*(sizeof(rec_t)));
-        rn1 = (rec_t *)(fp1 + count1*(sizeof(rec_t)));
-        rn2 = (rec_t *)(fp2 + count2*(sizeof(rec_t)));
+        rn1 = (srec_t *)(fp1 + count1*(sizeof(srec_t)));
+        rn2 = (srec_t *)(fp2 + count2*(sizeof(srec_t)));
 
         if (r->key < lowval || r->key > highval)
             continue;
 
         if (r->key < median) {
-            memcpy(rn1, r, sizeof(rec_t));
+            rn1->key = r->key;
+            rn1->precord = r->record;
             count1++;
+            //printf("%d %d %llu %u %p\n", __LINE__, getpid(), rn1->precord, rn1->key, rn1);
         } else {
-            memcpy(rn2, r, sizeof(rec_t));
+            rn2->key = r->key;
+            rn2->precord = r->record;
             count2++;
+            //printf("%d %d %llu %u %p\n", __LINE__, getpid(), rn2->precord, rn2->key, rn2);
         }
 
     }
 
-    printf("count1 %d, count2 %d total %d %lu\n", count1, count2, count1+ count2 +2, sizeof(unsigned int));
-    munmap(f, fs.st_size);
-    (void) close(fd);
-
+    printf("count1 %d, count2 %d total %d %lu %lu\n", count1, count2, count1+ count2, sizeof(unsigned int), sizeof(void *));
     lowindex = 0;
 
     // open and create output file
@@ -147,37 +147,82 @@ main(int argc, char *argv[])
 	perror("open");
 	exit(1);
     }
-
+    
+    //printf("pid parent %d\n",getpid());
     pid = fork();
 
     if (pid < 0) {
         perror("fork() error");
         exit(1);
     } else if (pid == 0) {
-        printf("parent start\n");
-        qsort(fp1, count1, sizeof(rec_t), compar_f);
-        rc = pwrite(fd, fp1, (count1) * sizeof(rec_t) , 0);
+        //printf("parent start\n");
+        char * sfp1 = (char *)malloc(count1 * sizeof(rec_t));
+        int l;
+        rec_t *fr;
+        srec_t *sr;
+        
+        if (sfp1 == NULL) {
+            //printf("%d %d\n", __LINE__, getpid());
+            exit(1);
+        }
+        //printf("%d %d\n", __LINE__, getpid());
+        qsort(fp1, count1, sizeof(srec_t), compar_f);
+        //printf("%d %d\n", __LINE__, getpid());
+#if 1
+        for (l = 0; l<count1; l++) {
+            sr = (srec_t *)(fp1 + l*(sizeof(srec_t)));
+            fr = (rec_t *)(sfp1 + l*(sizeof(rec_t)));
+            fr->key = sr->key;
+            memcpy(fr->record, sr->precord, NUMRECS);
+        }
+#endif
+        //printf("%d %d\n", __LINE__, getpid());
+
+        rc = pwrite(fd, sfp1, (count1) * sizeof(rec_t) , 0);
         if (rc != (count1) * sizeof(rec_t)) {
             perror("main write");
             exit(1);
         }
-        printf("parent end\n");
+        //printf("parent end\n");
     } else {
-        printf("pch start\n");
-        qsort(fp2, count2, sizeof(rec_t), compar_f);
+        //printf("pch start\n");
+        char * sfp2 = (char *)malloc(count2 * sizeof(rec_t));
+        int k;
+        rec_t *fr1;
+        srec_t *sr1;
+
+        //printf("%d %d\n", __LINE__, getpid());
+        qsort(fp2, count2, sizeof(srec_t), compar_f);
+        //printf("%d %d\n", __LINE__, getpid());
+
+        for (k = 0; k<count2; k++) {
+            sr1 = (srec_t *)(fp2 + k*(sizeof(srec_t)));
+            fr1 = (rec_t *)(sfp2 + k*(sizeof(rec_t)));
+            //printf("%d %p %u\n", __LINE__, sr1, sr1->key);
+
+            fr1->key = sr1->key;
+            memcpy(fr1->record, sr1->precord, NUMRECS);
+        }
+
+        //printf("%d %d\n", __LINE__, getpid());
+#if 1
         rc = pwrite(fd, fp2, (count2) * sizeof(rec_t), (count1) * sizeof(rec_t));
         if (rc != (count2) * sizeof(rec_t)) {
             perror("child write");
             exit(1);
         }
-        printf("pch end\n");
+#endif
+        //printf("pch end\n");
 	}
 
     pid = wait(&status);
+    printf("%d %d status = %d\n", __LINE__, getpid(), status);
     close(fd);
     free(fp1);
     free(fp2);
+    munmap(f, fs.st_size);
+    (void) close(ofd);
+
 
     return 0;
 }
-
