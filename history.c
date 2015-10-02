5e8e16b79eafdde25f3e223e7db021129aa5adbb
Mihir Shete <mihirsht@gmail.com>
2015-10-02 17:32:27 -0500

    fix history addition and printing

--- history.c
+++ history.c
@@ -8,20 +8,18 @@ ll list;
 void init_ll()
 {
 	list.head = 0;
-	list.tail = 0;
+	list.tail = -1;
 	list.counter = 0;
 }
 
 
 void add_cmd(char *cmd)
 {
-	ll_node node;
-	strcpy(node.cmd, cmd);
-	node.num = strlen(cmd);
 	list.tail++;
 	list.tail = list.tail % MAX_CMD_SIZE;
-	list.cmd_list[list.tail] = node;
+	strcpy(list.cmd_list[list.tail].cmd, cmd);
 	list.counter++;
+	list.cmd_list[list.tail].num = list.counter;
 	if(list.counter >= MAX_CMD_SIZE) {
 		list.head++;
 		list.head = list.head % MAX_CMD_SIZE;
@@ -45,7 +43,7 @@ void display_all_cmd()
         for (i = list.head; i < MAX_CMD_SIZE; i++) {
             printf("%d %s", list.cmd_list[i].num, list.cmd_list[i].cmd);
         }
-        for (i = 0; i < list.tail; i++) {
+        for (i = 0; i <= list.tail; i++) {
             printf("%d %s", list.cmd_list[i].num, list.cmd_list[i].cmd);
         }
     } else {
